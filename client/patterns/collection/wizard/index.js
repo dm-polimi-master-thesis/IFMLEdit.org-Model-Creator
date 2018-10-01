e514932bf977872c47bf85aea4ee8eb7135e2c3e
Unknown <davide.molinelli@mail.polimi.it>
2018-10-01 19:39:27 +0200

    Modal Pattern - work in progress

--- client/patterns/collection/wizard/index.js
+++ client/patterns/collection/wizard/index.js
@@ -3,27 +3,72 @@ function SettingsPatternViewModel(options) {
   var self = this;
 
   self.id = options.id;
-  self.steps = ko.observableArray([{ name: "Step 1", form: "Form 1", fileds: [] }, { name: "Step 2", form: "Form 2", fields: [] }]);
+  self.name = ko.observable();
+  self.stepToAdd = ko.observable("");
+  self.fieldToAdd = ko.observable("");
+  self.steps = ko.observableArray([{ name: "Step 1", form: "Step 1 Form", fields: [] }, { name: "Step 2", form: "Step 2 Form", fields: [] }]);
+  self.fields = ko.observableArray([]);
   self.selected = ko.observable(self.steps()[0]);
 
   self.addStep = function () {
     console.log("Add Step");
+    if(!(self.stepToAdd().length > 0)){
+      $.notify({message: 'Void string is not accepted as step name.'},
+        {allow_dismiss: true, type: 'danger'});
+    } else {
+
+      var name = self.stepToAdd();
+      var form = name + "Form";
+
+      ko.utils.arrayForEach(self.steps(), function(step) {
+         if(step.name === name){
+           $.notify({message: 'Duplicate step name is not accepted.'},
+             {allow_dismiss: true, type: 'danger'});
+           return;
+         }
+      })();
+
+      self.steps.push({ name: name, form: form, fields: [] });
+      self.stepToAdd("");
+    }
   }
 
   self.addField = function () {
     console.log("Add Field");
+    if(!(self.fieldToAdd().length > 0)){
+      $.notify({message: 'Void string is not accepted as field name.'},
+        {allow_dismiss: true, type: 'danger'});
+    } else {
+      
+      ko.utils.arrayForEach(self.fields(), function(step) {
+         if(field.name === name){
+           $.notify({message: 'Duplicate field name is not accepted.'},
+             {allow_dismiss: true, type: 'danger'});
+           return;
+         }
+      })();
+      var name = self.fieldToAdd();
+      self.fields.push({ name: name });
+      self.fieldToAdd("");
+    }
   }
 
   self.deleteStep = function () {
     console.log("Delete Step");
+    self.steps.remove(this);
   }
 
   self.deleteField = function () {
     console.log("Delete Field");
+    self.fields.remove(this);
   }
 
   self.addPatternName = function () {
     console.log("Add Pattern Name");
+  }
+
+  self.select = function () {
+    self.selected(this);
   }
 
   self.load = function () {
